130f5f981966cc536aab86de93721bd59b904e0e
Sarada <ryoyukikazu11@gmail.com>
2020-03-30 12:38:10 +0900

    update:ログ出力処理追加
    メソッド開始時時のログ出力処理を追加

--- src/main/java/com/example/diary/aspect/LogAspect.java
+++ src/main/java/com/example/diary/aspect/LogAspect.java
@@ -1,4 +1,16 @@
 package com.example.diary.aspect;
 
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
 public class LogAspect {
+
+    @Before("execution(* *..*.*Controller.*(..))")
+    public void startLog(JoinPoint joinPoint){
+        System.out.println("メソッド開始" +joinPoint.getSignature());
+    }
 }
