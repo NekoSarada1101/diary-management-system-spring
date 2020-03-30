2bc22a2ecaf350d665fd0577684ba9420c64f7c4
Sarada <ryoyukikazu11@gmail.com>
2020-03-30 12:18:43 +0900

    update:post通信の処理

--- src/main/java/com/example/diary/controller/LoginController.java
+++ src/main/java/com/example/diary/controller/LoginController.java
@@ -3,12 +3,18 @@ package com.example.diary.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class LoginController {
 
     @GetMapping("/login")
-    public String getLogin(Model model){
+    public String getLogin(Model model) {
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String postLogin(Model model) {
         return "login";
     }
 }
