4da2e898ab9d3b1300c84626baa970d73d37c69a
Sarada <ryoyukikazu11@gmail.com>
2020-03-31 12:45:40 +0900

    create:ユーザーデータを保持するクラス

---
+++ src/main/java/com/example/diary/domain/model/User.java
@@ -0,0 +1,11 @@
+package com.example.diary.domain.model;
+
+import lombok.Data;
+
+@Data
+public class User {
+    private String studentId;
+    private String classCode;
+    private String studentName;
+    private String studentPassword;
+}
