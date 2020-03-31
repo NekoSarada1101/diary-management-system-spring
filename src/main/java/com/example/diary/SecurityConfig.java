08c705c35cc357bbd0f4bcee8ece3cf5f3cb56e8
Sarada <ryoyukikazu11@gmail.com>
2020-03-31 13:27:53 +0900

    update:ログイン処理追加

--- src/main/java/com/example/diary/SecurityConfig.java
+++ src/main/java/com/example/diary/SecurityConfig.java
@@ -19,11 +19,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
                 .authorizeRequests()
-                //アクセス許可
-                .antMatchers("/webjars/**").permitAll()
-                .antMatchers("/css/**").permitAll()
-                .antMatchers("/login/**").permitAll()
+                .antMatchers("/webjars/**").permitAll() //アクセス許可
+                .antMatchers("/css/**").permitAll() //アクセス許可
+                .antMatchers("/login").permitAll() //login.htmlへの直リンク許可
                 .anyRequest().authenticated();//上記以外へのアクセス不許可
+
+        httpSecurity
+                .formLogin()
+                .loginProcessingUrl("/login") //ログイン処理のパス
+                .loginPage("/login") //ログインページの指定
+                .failureUrl("/login") //ログイン失敗時の遷移先
+                .usernameParameter("userId") //ログインページのユーザーID
+                .passwordParameter("password") //ログインページのパスワード
+                .defaultSuccessUrl("/main", true); //ログイン成功時の遷移先
 
         //一時的に
         httpSecurity.csrf().disable();
