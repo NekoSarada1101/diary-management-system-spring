50cc3f8466881795555ba7e86a3dcdd7b019f30c
Sarada <ryoyukikazu11@gmail.com>
2020-04-02 01:12:28 +0900

    create:学生用Daoクラス

---
+++ src/main/java/com/example/diary/domain/repository/jdbc/StudentDaoJdbc.java
@@ -0,0 +1,19 @@
+package com.example.diary.domain.repository.jdbc;
+
+import com.example.diary.domain.model.Student;
+import com.example.diary.domain.repository.StudentDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class StudentDaoJdbc implements StudentDao {
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    @Override
+    public Student login() {
+        return null;
+    }
+}
