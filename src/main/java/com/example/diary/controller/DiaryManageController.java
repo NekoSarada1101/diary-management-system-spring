008b8c97c1ae4b4d2579aea3bdd5457671361e35
Sarada <ryoyukikazu11@gmail.com>
2020-04-02 13:30:22 +0900

    create:日誌管理画面で日誌一覧を表示する

--- src/main/java/com/example/diary/controller/DiaryManageController.java
+++ src/main/java/com/example/diary/controller/DiaryManageController.java
@@ -1,16 +1,29 @@
 package com.example.diary.controller;
 
+import com.example.diary.domain.model.Diary;
+import com.example.diary.domain.service.DiaryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 public class DiaryManageController {
 
+    @Autowired
+    DiaryService diaryService;
 
     @GetMapping("/diaryManage")
     public String getDiaryManage(Model model) {
+        model.addAttribute("contents", "student/diaryList :: diaryList_contents");
+
+        List<Diary> diaryList = diaryService.fetchDiaryAll();
+
+        model.addAttribute("diaryList", diaryList);
+
         return "student/diaryManage";
     }
 
