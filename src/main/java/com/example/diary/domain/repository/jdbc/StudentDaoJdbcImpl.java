e593389375ba5414afdf603b38168c535f8f1390
Sarada <ryoyukikazu11@gmail.com>
2020-04-07 21:50:00 +0900

    update:ログイン処理のデータベースアクセス部分
    daoとserviceとjdbcの部分

--- src/main/java/com/example/diary/domain/repository/jdbc/StudentDaoJdbcImpl.java
+++ src/main/java/com/example/diary/domain/repository/jdbc/StudentDaoJdbcImpl.java
@@ -3,8 +3,11 @@ package com.example.diary.domain.repository.jdbc;
 import com.example.diary.domain.model.Student;
 import com.example.diary.domain.repository.StudentDao;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+
+import java.util.Map;
 
 @Repository
 public class StudentDaoJdbcImpl implements StudentDao {
@@ -13,7 +16,15 @@ public class StudentDaoJdbcImpl implements StudentDao {
     JdbcTemplate jdbcTemplate;
 
     @Override
-    public Student login() {
-        return null;
+    public Student login(String studentId, String password) throws DataAccessException {
+        Map<String, Object> studentInfo = jdbcTemplate.queryForMap("SELECT * FROM student WHERE student_id = ? AND student_password = ?", studentId, password);
+
+        Student student = new Student();
+
+        student.setStudentId((String) studentInfo.get("student_id"));
+        student.setClassCode((String) studentInfo.get("class_code"));
+        student.setStudentName((String) studentInfo.get("student_Name"));
+
+        return student;
     }
 }
